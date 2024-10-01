ec9c60b9b695c208a98fb2dbfe50f1aff0afdb2b
Arthur Silveira da Silva - 12922113367 <12922113367@ulife.com.br>
2024-10-01 19:47:27 -0300

    retornano matrix original

--- Objects/matrix.py
+++ Objects/matrix.py
@@ -1,36 +1,22 @@
-class Matrix():
+def matrix(l, c):
+    line = [0] * c
+    matrix = [line] * l
 
-    def __init__(self, lines, colunes):
-        self.lines = lines
-        self.colunes = colunes
-        self.matrix = [[0 for _ in range(colunes)] for _ in range(lines)]
+    for i in range(l):
+        line = []
+        for j in range(c):
+            number = int(input(f"Número ({i},{j}): "))
+            line.append(number)
+        matrix[i] = line
 
-    def get(self):
-        return self.matrix
-    
-    def get_value(self,value, l, c):
-        return self.matrix[l][c]
+    return matrix
 
-    def set_value(self, value, l, c):
-        self.matrix[l][c] = value
 
-    def transpose(self):
-        result = []
-        for i in range(len(self.matrix[0])):
-            line = []
-            for j in range(len(self.matrix)):
-                line.append(self.matrix[j][i]) 
-            result.append(line)
-        return result
-
-    def get_determinant(self):
-        if self.lines != self.colunes:
-            raise ValueError("Determinante só pode ser calculado para matrizes quadradas.")
-        elif self.lines == 2:
-            return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
-        elif self.lines == 3 :
-            return (self.matrix[0][0] * (self.matrix[1][1] * self.matrix[2][2] - self.matrix[1][2] * self.matrix[2][1]) -
-                    self.matrix[0][1] * (self.matrix[1][0] * self.matrix[2][2] - self.matrix[1][2] * self.matrix[2][0]) +
-                    self.matrix[0][2] * (self.matrix[1][0] * self.matrix[2][1] - self.matrix[1][1] * self.matrix[2][0]))
-        else:
-            pass
+def transpose(matrix):
+    result = []
+    for i in range(len(matrix[0])):
+        line = []
+        for j in range(len(matrix)):
+            line.append(matrix[j][i]) 
+        result.append(line)
+    return result
