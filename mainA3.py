df29b0c6b06f6508eca7ed569e288d6ef471e23d
Lucas Lança <63751427+LucasLanca@users.noreply.github.com>
2024-10-04 16:34:56 -0300

    Update mainA3.py

--- mainA3.py
+++ mainA3.py
@@ -1,3 +1,7 @@
+# 3. Faça um algoritmo em que o usuário define o tamanho da matriz e a preenche por completo com dados. A partir dessa matriz o algoritmo deve criar uma matriz do “tipo diagonal”.
+# A matriz diagonal é quadrada e todos os elementos fora da diagonal principal são iguais a 0.
+# Obrigatório: Utilizar o formato da matriz feita em sala de aula (aula05-exercícios). Não utilizar bibliotecas e funções do python.
+
 # Definir o tamanho da matriz
 n = int(input("Quantidade de Linhas da primeira matriz: "))
 m = int(input("Quantidade de Colunas da primeira matriz: "))
