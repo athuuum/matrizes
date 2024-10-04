593b4cbcb422a3b22b8d0c612cb73411e996b648
Lucas Lança <63751427+LucasLanca@users.noreply.github.com>
2024-10-04 16:35:23 -0300

    Update mainA4.py

--- mainA4.py
+++ mainA4.py
@@ -1,3 +1,7 @@
+# 4. Faça um algoritmo em que o usuário define o tamanho da matriz e o algoritmo gera uma matriz do “tipo identidade”. É uma matriz quadrada onde todos os elementos da diagonal
+# principal são iguais a 1 e todos os elementos fora dessa diagonal são iguais a 0.
+# Obrigatório: Utilizar o formato da matriz feita em sala de aula (aula05-exercícios). Não utilizar bibliotecas e funções do python.
+
 # Definir o tamanho da matriz
 n = int(input("Quantidade de Linhas da primeira matriz: "))
 m = int(input("Quantidade de Colunas da primeira matriz: "))
